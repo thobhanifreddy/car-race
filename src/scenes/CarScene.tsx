386d565371fdcf1d3b3ea25ce33c53721dbbef84
Freddy Thobhani <thobhani.freddy@gmail.com>
2023-11-15 20:25:01 +0100

    ✨ make it feel like car is moveing on the road

--- src/scenes/CarScene.tsx
+++ src/scenes/CarScene.tsx
@@ -16,7 +16,7 @@ const CarScene: React.FC = () => {
     const scene = new THREE.Scene();
 
     // Axes Helper
-    const axesHelper = new THREE.AxesHelper(5);
+    const axesHelper = new THREE.AxesHelper(10);
     scene.add(axesHelper);
 
     // Camera
@@ -26,12 +26,23 @@ const CarScene: React.FC = () => {
       0.1,
       100
     );
-    camera.position.z = 10;
+
+    // Adjust the camera position
+    camera.position.z = 4; // Closer to the car
+    camera.position.y = 2; // Higher than the car
 
     // Renderer
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current?.appendChild(renderer.domElement);
+
+    // Road Geometry
+    const roadGeometry = new THREE.PlaneGeometry(10, 100); // Width and length of the road
+    const roadMaterial = new THREE.MeshBasicMaterial({ color: 0x808080 }); // Grey color for the road
+    const road = new THREE.Mesh(roadGeometry, roadMaterial);
+    road.rotation.x = -Math.PI / 2; // Rotate the plane to be flat on the X-axis
+    road.position.y = -0.5; // Position it half the car's height below the car so it looks like it's sitting on the road
+    scene.add(road);
 
     // Car Geometry
     const geometry = new THREE.BoxGeometry(1, 0.5, 3);
@@ -42,6 +53,8 @@ const CarScene: React.FC = () => {
     const car = new THREE.Mesh(geometry, material);
     scene.add(car);
     carRef.current = car; // Reference the car
+
+    camera.lookAt(car.position);
 
     // Handling key events
     const onKeyDown = (event: KeyboardEvent) => {
