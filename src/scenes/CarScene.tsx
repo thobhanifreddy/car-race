7a5b8d5fc8de80ce648ca0a76bff612d9aa34cbb
Freddy Thobhani <thobhani.freddy@gmail.com>
2023-11-15 20:10:28 +0100

    ✨ moving rectabgle left right and in and out

--- src/scenes/CarScene.tsx
+++ src/scenes/CarScene.tsx
@@ -3,19 +3,30 @@ import * as THREE from "three";
 
 const CarScene: React.FC = () => {
   const mountRef = useRef<HTMLDivElement>(null);
+  const carRef = useRef<THREE.Mesh>(new THREE.Mesh());
+  const moveRef = useRef({
+    forward: false,
+    backward: false,
+    left: false,
+    right: false,
+  });
 
   useEffect(() => {
     // Scene
     const scene = new THREE.Scene();
+
+    // Axes Helper
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
 
     // Camera
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      100
     );
-    camera.position.z = 5;
+    camera.position.z = 10;
 
     // Renderer
     const renderer = new THREE.WebGLRenderer();
@@ -23,14 +34,63 @@ const CarScene: React.FC = () => {
     mountRef.current?.appendChild(renderer.domElement);
 
     // Car Geometry
-    const geometry = new THREE.BoxGeometry(1, 0.5, 2);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const geometry = new THREE.BoxGeometry(1, 0.5, 3);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: false,
+    });
     const car = new THREE.Mesh(geometry, material);
     scene.add(car);
+    carRef.current = car; // Reference the car
+
+    // Handling key events
+    const onKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowUp":
+          moveRef.current.forward = true;
+          break;
+        case "ArrowDown":
+          moveRef.current.backward = true;
+          break;
+        case "ArrowLeft":
+          moveRef.current.left = true;
+          break;
+        case "ArrowRight":
+          moveRef.current.right = true;
+          break;
+      }
+    };
+
+    const onKeyUp = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowUp":
+          moveRef.current.forward = false;
+          break;
+        case "ArrowDown":
+          moveRef.current.backward = false;
+          break;
+        case "ArrowLeft":
+          moveRef.current.left = false;
+          break;
+        case "ArrowRight":
+          moveRef.current.right = false;
+          break;
+      }
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
 
     // Render Loop
     const animate = () => {
       requestAnimationFrame(animate);
+
+      // Update car position based on state
+      if (moveRef.current.forward) carRef.current.position.z -= 0.01;
+      if (moveRef.current.backward) carRef.current.position.z += 0.01;
+      if (moveRef.current.left) carRef.current.position.x -= 0.01;
+      if (moveRef.current.right) carRef.current.position.x += 0.01;
+
       renderer.render(scene, camera);
     };
     animate();
@@ -47,6 +107,9 @@ const CarScene: React.FC = () => {
 
     return () => {
       window.removeEventListener("resize", handleResize);
+      window.removeEventListener("keydown", onKeyDown);
+      window.removeEventListener("keyup", onKeyUp);
+
       mountRef.current?.removeChild(renderer.domElement);
     };
   }, []);
