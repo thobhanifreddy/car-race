dd20d392f7ef5ac6435f578d9b94b5d52ab37646
Freddy Thobhani <thobhani.freddy@gmail.com>
2023-11-25 12:52:55 +0100

    ✨ new game screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./App.css";
+import { StartGame } from "./pages/StartGame";
 import CarScene from "./scenes/CarScene";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StartGame />,
+  },
+  {
+    path: "/game",
+    element: <CarScene />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <CarScene />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/StartGame.tsx
@@ -0,0 +1,16 @@
+export const StartGame = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Welcome to car race</h1>
+      <button>New Game</button>
+    </div>
+  );
+};
